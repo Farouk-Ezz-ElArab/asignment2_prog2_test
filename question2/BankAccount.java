4298a79182d964378be2716c69f01d590e4be325
Farouk-Ezz-ElArab <faroukezz9595@gmail.com>
2025-10-13 00:06:48 +0300

    finished part two

---
+++ question2/BankAccount.java
@@ -0,0 +1,61 @@
+package question2;
+
+public class BankAccount {
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolderName) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = 0;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit Amount: " + amount + " EGP");
+            System.out.println("New Balance: " + balance + " EGP\n");
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid withdrawal amount, Please enter a positive value.");
+        } else if (amount > balance) {
+            System.out.println("Insufficient funds. Withdrawal failed.");
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawal Amount: " + amount + " EGP");
+            System.out.println("New Balance: " + balance + " EGP\n");
+        }
+    }
+
+    public void checkBalance() {
+        System.out.println("Current Balance: " + balance + " EGP\n");
+    }
+    public void displayAccountDetails() {
+        System.out.println("Account Details:");
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder Name: " + accountHolderName);
+        System.out.println("Current Balance: " + balance + " EGP\n");
+    }
+}
---
+++ question2/TestBankAccount.java
@@ -0,0 +1,24 @@
+package question2;
+public class TestBankAccount {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount("123456", "John Doe");
+
+        System.out.println("Account Creation");
+        account.displayAccountDetails();
+
+        System.out.println("After Deposit");
+        account.deposit(1000);
+
+        System.out.println("After Withdrawal");
+        account.withdraw(500);
+
+        System.out.println("Checking Balance");
+        account.checkBalance();
+
+        System.out.println("Attempt to Withdraw More Than Balance");
+        account.withdraw(600);
+
+        System.out.println("Final Balance Check");
+        account.checkBalance();
+    }
+}
